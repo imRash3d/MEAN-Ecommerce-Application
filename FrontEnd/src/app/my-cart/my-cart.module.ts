4adaf69cce607b178df5006ba06cae5657cc00b9
rashed32 <rashedul.islam@selise.ch>
2019-07-08 20:44:56 +0600

    add routing

---
+++ FrontEnd/src/app/my-cart/my-cart.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyCartComponent } from './my-cart.component';
+
+@NgModule({
+  declarations: [MyCartComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MyCartModule { }
