0993e3e52646ab46939f0193796555cbaa4e20fd
rashed32 <rashedul.islam@selise.ch>
2019-07-10 20:23:26 +0600

    product create form

--- FrontEnd/src/app/shared-data/shared-modules.module.ts
+++ FrontEnd/src/app/shared-data/shared-modules.module.ts
@@ -5,12 +5,28 @@ import { ProductCardComponent } from "./components/product-card/product-card.com
 import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { FormsModule } from "@angular/forms";
-import {MatInputModule} from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatDatepickerModule} from '@angular/material/datepicker';
 @NgModule({
   declarations: [ProductCardComponent],
-  imports: [CommonModule, MatCardModule, FlexLayoutModule, FormsModule , MatInputModule , MatFormFieldModule],
-  exports: [ProductCardComponent, FlexLayoutModule , FormsModule , MatInputModule , MatFormFieldModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule
+  ],
+  exports: [
+    ProductCardComponent,
+    FlexLayoutModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule
+  ],
   providers: [ProductService]
 })
 export class SharedModulesModule {}
